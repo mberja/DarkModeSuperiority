13636e7752d58cf3fef135571d48e47c9fac281d
thienbn <britnienguyen@gmail.com>
2020-10-26 17:50:23 -0400

    jquery for location

---
+++ location-jquery.js
@@ -0,0 +1,15 @@
+$(function() {
+
+$("#container").hide();
+    
+$("#button").click(function() {
+    $("#container").toggle();
+})
+
+$("h2")
+    .wrapInner("<span>")
+$("h2 br")
+    .before("<span class='spacer'>")
+    .after("<span class='spacer'>");
+
+});
